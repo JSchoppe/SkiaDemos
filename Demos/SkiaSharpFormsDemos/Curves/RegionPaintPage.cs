cbb3bce40d12a8b55e1704c63aa5eef73a77cde4
JSchoppe <jethro.schoppenhorst@gmail.com>
2020-09-26 15:31:50 -0500

    adds comments stepping through non-transform examples

--- Demos/SkiaSharpFormsDemos/Curves/RegionPaintPage.cs
+++ Demos/SkiaSharpFormsDemos/Curves/RegionPaintPage.cs
@@ -13,6 +13,7 @@ namespace SkiaSharpFormsDemos.Curves
         {
             Title = "Region Paint";
 
+            // Creates the canvas and subscribes to its draw call.
             SKCanvasView canvasView = new SKCanvasView();
             canvasView.PaintSurface += OnCanvasViewPaintSurface;
             Content = canvasView;
@@ -20,10 +21,12 @@ namespace SkiaSharpFormsDemos.Curves
 
         void OnCanvasViewPaintSurface(object sender, SKPaintSurfaceEventArgs args)
         {
+            // Retrieve canvas state.
             SKImageInfo info = args.Info;
             SKSurface surface = args.Surface;
             SKCanvas canvas = surface.Canvas;
 
+            // Clear any drawing from previous draw call.
             canvas.Clear();
 
             int radius = 10;
@@ -36,11 +39,13 @@ namespace SkiaSharpFormsDemos.Curves
                 // Create circular region
                 using (SKRegion circleRegion = new SKRegion())
                 {
+                    // Defines a region that is 20 x 20 pixel units.
                     circleRegion.SetRect(new SKRectI(-radius, -radius, radius, radius));
                     circleRegion.SetPath(circlePath);
 
                     // Set transform to move it to center and scale up
                     canvas.Translate(info.Width / 2, info.Height / 2);
+                    // This scales the region to be proportional to our arbitrary units.
                     canvas.Scale(Math.Min(info.Width / 2, info.Height / 2) / radius);
 
                     // Fill region
@@ -49,8 +54,11 @@ namespace SkiaSharpFormsDemos.Curves
                         fillPaint.Style = SKPaintStyle.Fill;
                         fillPaint.Color = SKColors.Orange;
 
+                        // The circle appears pixelated because our
+                        // defined region defines the pixel count.
                         canvas.DrawRegion(circleRegion, fillPaint);
                     }
+                    // Fill paint is properly disposed of.
 
                     // Stroke path for comparison
                     using (SKPaint strokePaint = new SKPaint())
@@ -59,10 +67,15 @@ namespace SkiaSharpFormsDemos.Curves
                         strokePaint.Color = SKColors.Blue;
                         strokePaint.StrokeWidth = 0.1f;
 
+                        // The drawn path is not pixelated as it
+                        // is not bounded by a region.
                         canvas.DrawPath(circlePath, strokePaint);
                     }
+                    // Stoke paint is properly disposed of.
                 }
+                // Circle region is properly disposed of.
             }
+            // Circle path is properly disposed of.
         }
     }
 }
--- Demos/SkiaSharpFormsDemos/Paths/ArchimedeanSpiralPage.cs
+++ Demos/SkiaSharpFormsDemos/Paths/ArchimedeanSpiralPage.cs
@@ -7,12 +7,15 @@ using SkiaSharp.Views.Forms;
 
 namespace SkiaSharpFormsDemos.Paths
 {
+    // No .xaml is needed since the entire page
+    // is generated programatically.
     public class ArchimedeanSpiralPage : ContentPage
     {
         public ArchimedeanSpiralPage()
         {
             Title = "Archimedean Spiral";
 
+            // Creates the canvas and subscribes to its draw call.
             SKCanvasView canvasView = new SKCanvasView();
             canvasView.PaintSurface += OnCanvasViewPaintSurface;
             Content = canvasView;
@@ -20,25 +23,38 @@ namespace SkiaSharpFormsDemos.Paths
 
         void OnCanvasViewPaintSurface(object sender, SKPaintSurfaceEventArgs args)
         {
+            // Get information about the canvas state.
             SKImageInfo info = args.Info;
             SKSurface surface = args.Surface;
             SKCanvas canvas = surface.Canvas;
 
+            // Clear prior calls rendering.
             canvas.Clear();
 
+            // Define a coordinate at the screen center.
             SKPoint center = new SKPoint(info.Width / 2, info.Height / 2);
+            // Limit the radius based on the smaller screen dimension.
             float radius = Math.Min(center.X, center.Y);
 
+            // Ensure that the path object will be properly disposed
+            // of after the completion of this block.
             using (SKPath path = new SKPath())
             {
+                // The maths to draw the spiral.
                 for (float angle = 0; angle < 3600; angle += 1)
                 {
+                    // Calculate the current radius (expands at a linear rate).
                     float scaledRadius = radius * angle / 3600;
+                    // Calculate the current angle.
                     double radians = Math.PI * angle / 180;
+                    // Convert from angle to coords and add the center offset.
                     float x = center.X + scaledRadius * (float)Math.Cos(radians);
                     float y = center.Y + scaledRadius * (float)Math.Sin(radians);
                     SKPoint point = new SKPoint(x, y);
 
+                    // The first cycle of this loop must use move to
+                    // otherwise it will draw a line from the default
+                    // cursor position to the center of the screen.
                     if (angle == 0)
                     {
                         path.MoveTo(point);
@@ -49,6 +65,7 @@ namespace SkiaSharpFormsDemos.Paths
                     }
                 }
 
+                // Define the style of the path to draw.
                 SKPaint paint = new SKPaint
                 {
                     Style = SKPaintStyle.Stroke,
@@ -56,8 +73,10 @@ namespace SkiaSharpFormsDemos.Paths
                     StrokeWidth = 5
                 };
 
+                // Invoke the canvas to draw the defined.
                 canvas.DrawPath(path, paint);
             }
+            // At the closure of this block the generated path is disposed.
         }
     }
 }
