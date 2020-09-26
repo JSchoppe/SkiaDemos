737470cc89f1b2fc7957d542ddee70b5964765a5
JSchoppe <jethro.schoppenhorst@gmail.com>
2020-09-26 16:00:41 -0500

    adds comments walking thru transform examples

--- Demos/SkiaSharpFormsDemos/Transforms/BitmapScatterViewPage.xaml.cs
+++ Demos/SkiaSharpFormsDemos/Transforms/BitmapScatterViewPage.xaml.cs
@@ -12,6 +12,7 @@ using TouchTracking;
 
 namespace SkiaSharpFormsDemos.Transforms
 {
+    // xaml is used to define the touch interaction.
     public partial class BitmapScatterViewPage : ContentPage
     {
         List<TouchManipulationBitmap> bitmapCollection =
@@ -31,23 +32,30 @@ namespace SkiaSharpFormsDemos.Transforms
 
             foreach (string resourceID in resourceIDs)
             {
+                // Can resources for image files.
                 if (resourceID.EndsWith(".png") ||
                     resourceID.EndsWith(".jpg"))
                 {
+                    // Use a stream to read the files.
                     using (Stream stream = assembly.GetManifestResourceStream(resourceID))
                     {
+                        // Load each file into a bitmap object.
                         SKBitmap bitmap = SKBitmap.Decode(stream);
                         bitmapCollection.Add(new TouchManipulationBitmap(bitmap)
                         {
+                            // Create a new transformation matrix for this bitmap.
                             Matrix = SKMatrix.MakeTranslation(position.X, position.Y),
                         });
+                        // Add 100 pixels offset for each image generated.
                         position.X += 100;
                         position.Y += 100;
                     }
+                    // Properly dispose of the stream.
                 }
             }
         }
 
+        // Called when the user touches the screen.
         void OnTouchEffectAction(object sender, TouchActionEventArgs args)
         {
             // Convert Xamarin.Forms point to pixels
@@ -59,6 +67,7 @@ namespace SkiaSharpFormsDemos.Transforms
             switch (args.Type)
             {
                 case TouchActionType.Pressed:
+                    // Check for pressed hits from top to bottom.
                     for (int i = bitmapCollection.Count - 1; i >= 0; i--)
                     {
                         TouchManipulationBitmap bitmap = bitmapCollection[i];
@@ -69,9 +78,10 @@ namespace SkiaSharpFormsDemos.Transforms
                             bitmapCollection.Remove(bitmap);
                             bitmapCollection.Add(bitmap);
 
-                            // Do the touch processing
+                            // Keep track of this drag state.
                             bitmapDictionary.Add(args.Id, bitmap);
                             bitmap.ProcessTouchEvent(args.Id, args.Type, point);
+                            // Force the canvas to refresh.
                             canvasView.InvalidateSurface();
                             break;
                         }
@@ -100,11 +110,14 @@ namespace SkiaSharpFormsDemos.Transforms
             }
         }
 
+        // Runs every time the canvas makes a draw call.
         void OnCanvasViewPaintSurface(object sender, SKPaintSurfaceEventArgs args)
         {
             SKCanvas canvas = args.Surface.Canvas;
+            // Clear what was previously on the canvas.
             canvas.Clear();
 
+            // Redraw each of the bitmaps.
             foreach (TouchManipulationBitmap bitmap in bitmapCollection)
             {
                 bitmap.Paint(canvas);
--- Demos/SkiaSharpFormsDemos/Transforms/UglyAnalogClockPage.cs
+++ Demos/SkiaSharpFormsDemos/Transforms/UglyAnalogClockPage.cs
@@ -16,18 +16,22 @@ namespace SkiaSharpFormsDemos.Transforms
         {
             Title = "Ugly Analog Clock";
 
+            // Initialize the canvas stuff and bind to draw call.
             canvasView = new SKCanvasView();
             canvasView.PaintSurface += OnCanvasViewPaintSurface;
             Content = canvasView;
         }
 
+        // Handles initialization of timer.
         protected override void OnAppearing()
         {
             base.OnAppearing();
             pageIsActive = true;
 
+            // Create a timer that ticks every second.
             Device.StartTimer(TimeSpan.FromSeconds(1), () =>
             {
+                // Force a new draw call.
                 canvasView.InvalidateSurface();
                 return pageIsActive;
             });
@@ -41,19 +45,20 @@ namespace SkiaSharpFormsDemos.Transforms
 
         void OnCanvasViewPaintSurface(object sender, SKPaintSurfaceEventArgs args)
         {
+            // Retrieve canvas state.
             SKImageInfo info = args.Info;
             SKSurface surface = args.Surface;
             SKCanvas canvas = surface.Canvas;
-
+            // Clear anything drawn in previous calls.
             canvas.Clear();
 
             using (SKPaint strokePaint = new SKPaint())
             using (SKPaint fillPaint = new SKPaint())
             {
+                // Define clock styling.
                 strokePaint.Style = SKPaintStyle.Stroke;
                 strokePaint.Color = SKColors.Black;
                 strokePaint.StrokeCap = SKStrokeCap.Round;
-
                 fillPaint.Style = SKPaintStyle.Fill;
                 fillPaint.Color = SKColors.Gray;
 
@@ -64,7 +69,9 @@ namespace SkiaSharpFormsDemos.Transforms
                 // Hour and minute marks
                 for (int angle = 0; angle < 360; angle += 6)
                 {
+                    // Draw the dot based on whether it is a one minute or five minute mark.
                     canvas.DrawCircle(0, -90, angle % 30 == 0 ? 4 : 2, fillPaint);
+                    // Pivot the canvas.
                     canvas.RotateDegrees(6);
                 }
 
@@ -72,10 +79,10 @@ namespace SkiaSharpFormsDemos.Transforms
 
                 // Hour hand
                 strokePaint.StrokeWidth = 20;
-                canvas.Save();
+                canvas.Save(); // <-- We about to f*** everything up, give us an undo.
                 canvas.RotateDegrees(30 * dateTime.Hour + dateTime.Minute / 2f);
                 canvas.DrawLine(0, 0, 0, -50, strokePaint);
-                canvas.Restore();
+                canvas.Restore(); // Now we use that undo.
 
                 // Minute hand
                 strokePaint.StrokeWidth = 10;
@@ -91,6 +98,7 @@ namespace SkiaSharpFormsDemos.Transforms
                 canvas.DrawLine(0, 10, 0, -80, strokePaint);
                 canvas.Restore();
             }
+            // Properly dispose of stroke paint and fill paint.
         }
     }
 }
